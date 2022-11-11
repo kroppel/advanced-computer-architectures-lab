1ba20c72c4f2af2a4beaccf94261cf61d28d9a44
kroppel <konsti.roppel@t-online.de>
2022-11-11 11:40:56 +0100

    increase load in find exercise

--- Exercises_OMP1/Find/Find.cpp
+++ Exercises_OMP1/Find/Find.cpp
@@ -6,11 +6,13 @@
 
 int main() {
     using namespace timer;
-    int N = (1 << 25);
+    int N = (1 << 30);
 
     int* Array = new int[N];
     const int to_find1 = 18;        //search [ .. ][ .. ][ 18 ][ 64 ][ .. ]
     const int to_find2 = 64;
+    const int to_find3 = 20;
+
 
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::chrono::time_point<std::chrono::system_clock> start, end;
@@ -25,8 +27,8 @@ int main() {
     TM.start();
 
     int index = -1;
-    for (int i = 0; i < N - 1; ++i) {
-        if (Array[i] == to_find1 && Array[i + 1] == to_find2) {
+    for (int i = 0; i < N - 2; ++i) {
+        if (Array[i] == to_find1 && Array[i + 1] == to_find2 && Array[i + 2] == to_find3) {
             index = i;
             break;            // !!! error in OPENMP
         }
@@ -48,7 +50,7 @@ int main() {
         if (is_found) {
             continue;
         }
-        if (Array[i] == to_find1 && Array[i + 1] == to_find2) {
+        if (Array[i] == to_find1 && Array[i + 1] == to_find2 && Array[i + 2] == to_find3) {
             index = i;
             is_found = true;
         }
