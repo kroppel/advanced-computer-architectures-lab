044fb0a28ba86b5287454482efd2d5a127f1ed0b
kroppel <konsti.roppel@t-online.de>
2022-11-26 18:18:19 +0100

    Make Producer/Consumer model multithreaded with synchronization of buffer access and buffer size monitoring

--- Exercises_OMP2/ProducerConsumer/ProducerConsumer.cpp
+++ Exercises_OMP2/ProducerConsumer/ProducerConsumer.cpp
@@ -4,32 +4,35 @@
 #include <time.h>
 #include <omp.h>
 
-void test_producer_consumer(int Buffer[32]) {
+void test_producer_consumer(int Buffer[32], int * count) {
 	int i = 0;
-	int count = 0;
 
 	while (i < 35000) {					// number of test
 
 		// PRODUCER
 		if ((rand() % 50) == 0) {		// some random computations
-
-			if (count < 31) {
-				++count;
-				std::cout << "Thread:\t" << omp_get_thread_num()
-                          << "\tProduce on index: " << count << std::endl;
-				Buffer[count] = omp_get_thread_num();
+#pragma omp critical
+			{
+				if (*count < 31) {
+					++*count;
+					std::cout << "Thread:\t" << omp_get_thread_num()
+							<< "\tProduce on index: " << *count << std::endl;
+					Buffer[*count] = omp_get_thread_num();
+				}
 			}
 		}
 
 		// CONSUMER
 		if ((std::rand() % 51) == 0) {		// some random computations
-
-			if (count >= 1) {
-				int var = Buffer[count];
-				std::cout << "Thread:\t" << omp_get_thread_num()
-                          << "\tConsume on index: " << count
-                          << "\tvalue: " << var << std::endl;
-				--count;
+#pragma omp critical
+			{
+				if (*count >= 1) {
+					int var = Buffer[*count];
+					std::cout << "Thread:\t" << omp_get_thread_num()
+							<< "\tConsume on index: " << *count
+							<< "\tvalue: " << var << std::endl;
+					--*count;
+				}
 			}
 		}
 		i++;
@@ -38,9 +41,13 @@ void test_producer_consumer(int Buffer[32]) {
 
 int main() {
 	int Buffer[32];
+	int count = 0;
 	std::srand(time(NULL));
 
 	omp_set_num_threads(2);
 
-	test_producer_consumer(Buffer);
+#pragma omp parallel 
+	{
+		test_producer_consumer(Buffer, &count);
+	}
 }
--- Exercises_OMP2/ProducerConsumer/compile.sh
+++ Exercises_OMP2/ProducerConsumer/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DIR=`dirname $0`
 
-g++-9 -std=c++11 -fopenmp "$DIR"/ProducerConsumer.cpp -o build/producerconsumer
+rm ./build/producerconsumer
+g++ -std=c++11 -fopenmp "$DIR"/ProducerConsumer.cpp -o build/producerconsumer
 ./build/producerconsumer
